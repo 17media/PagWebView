b301783606e1fb6f850bb29dc155392c3bff4f2b
ZackChiang <zack@17.media>
2022-12-16 11:59:45 +0800

    [Add] execute animation from url

--- app/src/main/java/com/machipopo/media17/WebActivity.kt
+++ app/src/main/java/com/machipopo/media17/WebActivity.kt
@@ -8,6 +8,7 @@ class WebActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWebBinding
 
+    private var listURL = listOf("http://192.168.0.116:8000/pag/assets/fire_phoenix.pag", "http://192.168.0.116:8000/pag/assets/healfarm.pag")
     private var listAssets = listOf("2205_tw_artemis.pag", "gift_vip_newking.pag", "fire_phoenix.pag", "healfarm.pag")
 
     private var index = 0
@@ -19,6 +20,13 @@ class WebActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btn.setOnClickListener {
+
+//            binding.web.executeAnimation(listURL[index])
+//            index++
+//            if(index >= listURL.size){
+//                index = 0
+//            }
+
             binding.web.readPagFile(listAssets[index])
             index++
             if(index >= listAssets.size){
